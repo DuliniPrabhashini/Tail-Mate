3f1ae6fc6dbaed267c018b98879dc57ad0b2ac05
Dulini Prabhashini <duliniprabhashini57@gmail.com>
2026-01-30 00:37:04 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const Index = () =>{
+  return(
+    <View>
+      <Text>
+        Hello tailmate
+      </Text>
+    </View>
+  )
+}
+
+export default Index
